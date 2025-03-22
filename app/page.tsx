516647602871532a77378fad4f7ef40a52b33ba4
akashkumar30698 <144131035+akashkumar30698@users.noreply.github.com>
2025-03-22 15:35:29 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 
-import LoginScreen from "./signin/page";
-import AccountSettings from "./account/page";
-import RegistrationScreen from "./signUp/page";
 import PopXLogin from "./(pages)/popx-login";
 
 export default function Home() {
